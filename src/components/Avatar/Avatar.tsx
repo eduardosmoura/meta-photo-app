5e7871157b279102653810db3dfc3f26672e706d
Eduardo Santiago Moura <eduardosmoura@gmail.com>
2025-02-01 21:31:35 -0300

    feat: refactor avatar component

---
+++ src/components/Avatar/Avatar.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+interface AvatarProps {
+  src: string;
+  alt: string;
+  className?: string;
+}
+
+const placeholderImage = '../../src/assets/logo.svg';
+
+export const Avatar: React.FC<AvatarProps> = ({ src, alt, className }) => {
+  const handleImageError = (
+    e: React.SyntheticEvent<HTMLImageElement, Event>
+  ) => {
+    e.currentTarget.src = placeholderImage;
+    console.error(`Image load failed for: ${alt}`);
+  };
+
+  return (
+    <img src={src} alt={alt} className={className} onError={handleImageError} />
+  );
+};
